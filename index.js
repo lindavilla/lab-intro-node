5090cb5fce3f6b45b0da162ece3ca96aa6f5df5b
Linda Villa <linda.villa@alea.com>
2021-04-18 23:48:33 +0200

    Complete Bonuses

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
 class SortedList {
-  constructor() {}
 
-  add(item) {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a-b);
+    this.length = this.length + 1;
+  }
 
-  max() {}
+  get(pos) {
+    if (pos < 0 || pos > this.length)
+      throw new Error('OutOfBounds');
+    else {
+      return this.items[pos];
+    }
+  }
 
-  min() {}
+  max() {
+    let highestValue = 0;
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else {
+      
+      for (let i = 0; i < this.items.length; i++){
+        if (this.items[i] > highestValue) {
+          highestValue = this.items[i];
+        }
+      }
+    }
+    return highestValue;
+  }
 
-  sum() {}
+  min() {
+    let lowestValue = null;
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+      }
+      else {
+        for (let i=0; i<this.items.length; i++) {
+          if (lowestValue === null || this.items[i] < lowestValue) {
+            lowestValue = this.items[i];
+          }
+        }
+      }
+      return lowestValue;
+  }
 
-  avg() {}
+  sum() {
+    let sum = 0;
+    for (let i=0; i<this.items.length; i++){
+      sum += this.items[i];
+    }
+    return sum;
+    }
+
+  avg() {
+
+    if (this.items.length === 0){
+      throw new Error('EmptySortedList');
+      }
+      else 
+        return this.sum() / this.items.length;
+  }
+
 }
 
 module.exports = SortedList;
+
